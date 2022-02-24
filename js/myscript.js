a26f1428708568f47de569a15025024c0f72cf89
alessandroordetti <alessandro.ordetti@gmail.com>
2022-02-24 19:17:44 +0100

    Correzione esercizio

--- js/myscript.js
+++ js/myscript.js
@@ -4,17 +4,21 @@ let wrapper = document.getElementById('wrapper-div');
 for (let i = 0; i < 101; i++) {
     let boxText = document.createElement ("div");
 
-    boxText.classList.add('col');
+    boxText.classList.add('col-2');
 
     wrapper.append(boxText);
 
     if (i % 15 == 0) {
         console.log("fizzbuzz");
+        boxText.innerHTML = "FizzBuzz";
     } else if (i % 5 == 0) {
         console.log("buzz");
+        boxText.innerHTML = "Buzz";
     } else if (i % 3 == 0) {
         console.log("fizz");
+        boxText.innerHTML = "Fizz"
     } else {
         console.log(i);
+        boxText.innerHTML = i
     }
 }
